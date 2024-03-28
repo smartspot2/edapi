e4185739dbc3245e28bc6ca6bc8694acf32f0aaf
Trevor Moy <51927676+trevmoy@users.noreply.github.com>
2024-03-28 14:55:48 -0400

    Implemented lessons.py and added some other things

--- edapi/__init__.py
+++ edapi/__init__.py
@@ -3,4 +3,4 @@ Package for Ed API Python integration.
 """
 
 # the only default import should be the API class
-from .edapi import EdAPI
+from .edapi import EdAPIWL
--- edapi/types/api_types/endpoints/lessons.py
+++ edapi/types/api_types/endpoints/lessons.py
@@ -1,1 +1,26 @@
 # TODO: Implement the lessons endpoint
+
+from typing import Any, Optional, TypedDict
+from ..user import API_User
+
+
+# === GET /api/lessons/<lesson_id> ===
+# also used by /api/courses/<course_id>/lessons/<lesson_number>?view=1
+
+class API_GetLesson(TypedDict):
+    """
+    Response type for GET /api/lessons/<lesson_id>.
+
+    Also used by GET /api/courses/<course_id>/lessons/<lesson_number>?view=1.
+    """
+    
+    # Lesson fields. Not all fields are included.
+    lesson: 53507 # temp class id
+    id: int
+    title: str
+    type: str 
+    kind: str 
+    openable: bool # Whether the lesson is open or closed
+    status: str 
+    due_at: str # ISO 8601 date-time string
+    created_at: str # ISO 8601 date-time string
